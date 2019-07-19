863eea94c9da40f2357a0f3f1ca896d54b1e901e
Brian Haferkamp <brian.haferkamp@gmail.com>
2019-07-19 11:20:38 -0500

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,7 +2,7 @@
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker
   // Register the path to the service worker file
-  .register('../../sw.js')
+  .register('./sw.js')
   .then(function() {
     console.log('Service Worker: Registered');
   });
